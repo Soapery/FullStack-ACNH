1008d9c42b5cf59f6cc31388da81ff0412c3bfb6
Soapery <darian.a.ronald@gmail.com>
2023-10-13 11:19:03 -0500

    Updated to properly reference player.

--- db/migrate/20231012162313_create_player_homes.rb
+++ db/migrate/20231012162313_create_player_homes.rb
@@ -1,7 +1,7 @@
 class CreatePlayerHomes < ActiveRecord::Migration[7.0]
   def change
     create_table :player_homes do |t|
-      t.references :player_id, null: false, foreign_key: true
+      t.references :player, null: false, foreign_key: true
       t.string :size, limit: 10
       t.integer :home_value
 
