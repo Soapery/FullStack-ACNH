67abfcaad38e763681060de5b5389db4364295ea
Soapery <darian.a.ronald@gmail.com>
2023-10-13 13:46:20 -0500

    Updated associations.

--- app/models/player_home.rb
+++ app/models/player_home.rb
@@ -1,6 +1,7 @@
 class PlayerHome < ApplicationRecord
   belongs_to :player
-  has_many :furniture, through: :home_furniture
+  has_many :home_furnitures, dependent: :destroy
+  has_many :furniture, through: :home_furnitures
 
   validates :player_id, :size, :home_value, presence: true
   validates :size,
