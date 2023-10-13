59dec50ab894fd7754b001ac6da060450175240f
Soapery <darian.a.ronald@gmail.com>
2023-10-12 23:11:00 -0500

    Adjusted redundant column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_12_162532) do
+ActiveRecord::Schema[7.0].define(version: 20_231_012_162_532) do
   create_table "furnitures", force: :cascade do |t|
     t.string "name", limit: 50
     t.string "variation", limit: 50
@@ -28,12 +28,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_12_162532) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["furniture_id"], name: "index_home_furnitures_on_furniture_id"
-    t.index ["home_id", "furniture_id"], name: "index_home_furnitures_on_home_id_and_furniture_id", unique: true
+    t.index ["home_id", "furniture_id"], name:   "index_home_furnitures_on_home_id_and_furniture_id",
+                                         unique: true
     t.index ["home_id"], name: "index_home_furnitures_on_home_id"
   end
 
   create_table "player_homes", force: :cascade do |t|
-    t.integer "player_id_id", null: false
+    t.integer "player_id", null: false
     t.string "size", limit: 10
     t.integer "home_value"
     t.datetime "created_at", null: false
