c9277a8e4f9c253c0ce8225583c05f10bd0bf02c
Soapery <darian.a.ronald@gmail.com>
2023-10-21 15:08:19 -0500

    Added routes for new controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,16 @@ Rails.application.routes.draw do
 
   # Add a route for the "about" page
   get 'about', to: 'about#index'
+
+  # Player routes
+  resources :player, only: [:index, :show], path: 'player'
+
+  # PlayerHome routes
+  resources :player_home, only: [:index, :show], path: 'player_home'
+
+  # HomeFurniture routes
+  resources :home_furniture, only: [:index, :show], path: 'home_furniture'
+
+  # Furniture routes
+  resources :furniture, only: [:index, :show], path: 'furniture'
 end
